af7d5a0173ec066e72affd42ab23072b9aa2f223
Akanksha1199 <akankshachaudhary076@gmail.com>
2024-06-02 20:12:15 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,9 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
         SUV fortuner = new SUV("Fortuner", false);
         fortuner.move(40,0);
         fortuner.accelerate(20);
-       // fortuner.accelerate(-60);
         System.out.println("Current Speed : " + fortuner.getCurrentSpeed());
         System.out.println("Current Gear : " + fortuner.getCurrentGear());
     }
